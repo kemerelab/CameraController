2da652c810854482c6bb944335acf78775c1e88b
Tam Vu <vu.tamd@gmail.com>
2014-07-07 16:09:30 -0500

    Update README and MSP430 Code

--- Firmware/README.md
+++ Firmware/README.md
@@ -1,2 +1,18 @@
+Overview: 
+This product is designed as a signal generator that can outputs square waves (3.3V high) to up to two audio jacks and two BNC connectors. It is controlled by a computer via serial communications.
 
-Code for the MSP430.
+Input: 1 Mini USB, 11 pin headers for unused FT232RL pins, 4 pin headers for programming MSP430
+Output: 2 Audio Jacks, 2 BNC connectors
+
+Major Internal Components:
+FT232RL - receives power and USB input from the computer, provides power and converts USB to serial communications between computer and MSP430, unused pins connected to pin headers
+MSP430G2553 - receives power and serial input from FT232RL, outputs square waves and returns input to FT232RL, can be programmed using pin headers
+TXLED - indicates when FT232RL is sending serial signals to MSP430
+RXLED - indicates when FT232RL is receiving serial signals from MSP430
+PWR_LED - indicates that the MSP430 is receiving power, and that the whole system is powered on
+CAM1_LED - flashes when a square wave is output to corresponding audio jack, off otherwise
+CAM2_LED - flashes when a square wave is output to corresponding audio jack, off otherwise
+SYNC1_LED - flashes when a square wave is output to corresponding BNC connector, off otherwise
+SYNC2_LED - flashes when a square wave is output to corresponding BNC connector, off otherwise
+
+Initial MSP430 code provided. Can be altered as needed.
---
+++ Firmware/msp430_code.c
@@ -0,0 +1,195 @@
+#include <msp430.h>
+#include "stdint.h"
+
+#define frequency 30 //Hz (30)
+#define pulse_delay 16000000/(2*frequency)
+
+const unsigned long smclk_freq = 16000000;      // SMCLK frequency in hertz
+const unsigned long bps = 9600;                 // Async serial bit rate
+int i = 0;										//counter
+int ALL = 0;								//flag
+int SYNC1 = 0;
+int SYNC2 = 0;
+int CAM1 = 0;
+int CAM2 = 0;
+int SIDE1 = 0;
+int SIDE2 = 0;
+int SYNC = 0;
+int CAM = 0;
+
+char test[18];
+
+void out(data){                                 //function to output characters through the hardware uart
+    while (!(IFG2 & UCA0TXIFG));
+    UCA0TXBUF = data;
+}
+
+void cameratest(void){					//test function. outputs 30 pulses
+	for (i=30;i>0;i--){
+		P2OUT ^= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
+		_delay_cycles(pulse_delay);
+
+		P2OUT ^= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
+		_delay_cycles(pulse_delay);
+	}
+}
+
+void main(void){
+	WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+	BCSCTL1 = CALBC1_16MHZ;						//run the 16 MHz clock
+	DCOCTL = CALDCO_16MHZ;
+	P1SEL = BIT1 + BIT2 ;                     // P1.1 = RXD, P1.2=TXD
+	P1SEL2 = BIT1 + BIT2 ;                     // P1.1 = RXD, P1.2=TXD
+	UCA0CTL1 |= UCSSEL_2;                     // SMCLK
+
+	const unsigned long brd = (smclk_freq + (bps >> 1)) / bps; // Bit rate divisor
+
+	UCA0BR1 = (brd >> 12) & 0xFF;               // High byte of whole divisor
+	UCA0BR0 = (brd >> 4) & 0xFF;                // Low byte of whole divisor
+	UCA0MCTL = ((brd << 4) & 0xF0) | UCOS16;    // Fractional divisor, oversampling mode
+
+	UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+	IE2 |= UCA0RXIE;                          // Enable USCI_A0 RX interrupt
+	__bis_SR_register(GIE);
+    P2SEL = 0;
+    P2SEL2 = 0;
+
+    P2DIR = BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
+    P2OUT = BIT0 + BIT2 + BIT4 + BIT6;
+
+    while (1){								//main loop. entered  is sent
+
+		while (ALL){
+				P2OUT ^= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
+				_delay_cycles(pulse_delay);
+				P2OUT ^= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
+				_delay_cycles(pulse_delay);
+			}
+
+		while (SYNC1){
+				P2OUT ^= BIT4 + BIT5;
+				_delay_cycles(pulse_delay);
+				P2OUT ^= BIT4 + BIT5;
+				_delay_cycles(pulse_delay);
+			}
+
+
+		while (SYNC2){
+				P2OUT ^= BIT6 + BIT7;
+				_delay_cycles(pulse_delay);
+				P2OUT ^= BIT6 + BIT7;
+				_delay_cycles(pulse_delay);
+			}
+
+
+		while (CAM1){
+				P2OUT ^= BIT0 + BIT1;
+				_delay_cycles(pulse_delay);
+				P2OUT ^= BIT0 + BIT1;
+				_delay_cycles(pulse_delay);
+			}
+
+		while (CAM2){
+				P2OUT ^= BIT2 + BIT3;
+				_delay_cycles(pulse_delay);
+				P2OUT ^= BIT2 + BIT3;
+				_delay_cycles(pulse_delay);
+			}
+
+		while (SIDE1){
+				P2OUT ^= BIT0 + BIT1 + BIT6 + BIT7;
+				_delay_cycles(pulse_delay);
+				P2OUT ^= BIT0 + BIT1 + BIT6 + BIT7;
+				_delay_cycles(pulse_delay);
+			}
+
+		while (SIDE2){
+				P2OUT ^= BIT2 + BIT3 + BIT4 + BIT5;
+				_delay_cycles(pulse_delay);
+				P2OUT ^= BIT2 + BIT3 + BIT4 + BIT5;
+				_delay_cycles(pulse_delay);
+			}
+
+		while (SYNC){
+				P2OUT ^= BIT4 + BIT5 + BIT6 + BIT7;
+				_delay_cycles(pulse_delay);
+				P2OUT ^= BIT4 + BIT5 + BIT6 + BIT7;
+				_delay_cycles(pulse_delay);
+			}
+
+		while (CAM){
+				P2OUT ^= BIT0 + BIT1 + BIT2 + BIT3;
+				_delay_cycles(pulse_delay);
+				P2OUT ^= BIT0 + BIT1 + BIT2 + BIT3;
+				_delay_cycles(pulse_delay);
+			}
+
+    }
+
+}
+
+#pragma vector=USCIAB0RX_VECTOR
+__interrupt void USCI0RX_ISR(void)
+{
+
+	 	out(UCA0RXBUF);             //return every character received
+
+	    if (UCA0RXBUF == 0x0d){         //if enter is pressed, check to see if a valid command has been input
+	        out('\r');
+	        out('\n');
+	        ALL = SYNC1 = SYNC2 = CAM1 = CAM2 = SIDE1 = SIDE2 = SYNC = CAM = 0;
+
+	        if ((test[0] == 't'||test[0] == 'T') && (test[1] == 's' || test[1] == 'S') && (test[2] == 'e' || test[2] == 'E') && (test[3] == 't' || test[3] == 'T')){ //outputs '30pulses@"x"hz
+	        	cameratest();
+	        	out('3');out('0');out('p');out('u');out('l');out('s');out('e');out('s');out('@');out(frequency / 10 +48); out(frequency % 10 + 48);out('h');out('z');out('\r');out('\n');
+	        }
+
+	        if ((test[0] == 'l'||test[0] == 'L') && (test[1] == 'l' || test[1] == 'L') && (test[2] == 'a' || test[2] == 'A')){		//starts pulses
+	        	ALL ^= 1;
+	        }
+
+	        if ((test[0] == '1') && (test[1]=='c' || test[1]=='C') && (test[2] == 'n' || test[2] == 'N') && (test[3] == 'y' || test[3] == 'Y') && (test[4] == 's' || test[4] == 'S')){
+	        	SYNC1 ^= 1;
+	        }
+
+	        if ((test[0] == '2') && (test[1]=='c' || test[1]=='C') && (test[2] == 'n' || test[2] == 'N') && (test[3] == 'y' || test[3] == 'Y') && (test[4] == 's' || test[4] == 'S')){
+	        	SYNC2 ^= 1;
+	        }
+
+	        if ((test[0] == '1') && (test[1]=='m' || test[1]=='M') && (test[2] == 'a' || test[2] == 'A') && (test[3] == 'c' || test[3] == 'C')){
+	        	CAM1 ^= 1;
+	        }
+
+	        if ((test[0] == '2') && (test[1]=='m' || test[1]=='M') && (test[2] == 'a' || test[2] == 'A') && (test[3] == 'c' || test[3] == 'C')){
+	        	CAM2 ^= 1;
+	        }
+
+	        if ((test[0] == '1') && (test[1]=='E' || test[1]=='e') && (test[2] == 'd' || test[2] == 'D') && (test[3] == 'i' || test[3] == 'I') && (test[4] == 's' || test[4] == 'S')){
+	        	SIDE1 ^= 1;
+	        }
+
+	        if ((test[0] == '2') && (test[1]=='E' || test[1]=='e') && (test[2] == 'd' || test[2] == 'D') && (test[3] == 'i' || test[3] == 'I') && (test[4] == 's' || test[4] == 'S')){
+	        	SIDE2 ^= 1;
+	        }
+
+	        if ((test[0]=='c' || test[0]=='C') && (test[1] == 'n' || test[1] == 'N') && (test[2] == 'y' || test[2] == 'Y') && (test[3] == 's' || test[3] == 'S')){
+	        	SYNC ^= 1;
+	        }
+
+	        if ((test[0]=='M' || test[0]=='m') && (test[1] == 'a' || test[1] == 'A') && (test[2] == 'c' || test[2] == 'C')){
+	        	CAM ^= 1;
+	        }
+
+
+
+	    }
+	    for (i = 17; i>0; i--){      //shift the array that stores the character inputs and input the new character at the bottom
+	            test[i]=test[i-1];
+		}
+		test[0]=UCA0RXBUF;
+
+}
+
+
+
+
