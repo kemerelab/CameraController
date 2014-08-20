0ed9939d735dd6b92687eeeb9b488eb930e206ba
Tam Vu <vu.tamd@gmail.com>
2014-08-20 17:04:22 -0500

    Commented MSP430 Code
    Commented MSP430 code, mainly for Etienne

--- Firmware/msp430_code.c
+++ Firmware/msp430_code.c
@@ -4,9 +4,9 @@
 
 const unsigned long smclk_freq = 16000000;      // SMCLK frequency in hertz
 const unsigned long bps = 9600;                 // Async serial bit rate
-int freq = 30;
+int freq = 30;									// Initial frequency when started up, can be changed using command-line interface
 int i = 0;										//counter
-int ALL = 0;								//flag
+int ALL = 0;								//flag for which "state" the controller is in
 int SYNC1 = 0;
 int SYNC2 = 0;
 int CAM1 = 0;
@@ -16,14 +16,14 @@ int SIDE2 = 0;
 int SYNC = 0;
 int CAM = 0;
 
-char test[18];
+char test[18];									//array for storing most recent input command
 
 void out(data){                                 //function to output characters through the hardware uart
     while (!(IFG2 & UCA0TXIFG));
     UCA0TXBUF = data;
 }
 
-void pulse_delay(freq){
+void pulse_delay(freq){							//return a delay of the width of one pulse, actual time depends on the current frequency
 	if (freq == 30){
 		_delay_cycles(16000000/60);
 	}
@@ -42,7 +42,7 @@ void pulse_delay(freq){
 }
 
 void cameratest(void){					//test function. outputs 30 pulses
-	for (i=100;i>0;i--){
+	for (i=30;i>0;i--){
 		P2OUT ^= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
 		pulse_delay(freq);
 
@@ -56,7 +56,7 @@ void main(void){
 	BCSCTL1 = CALBC1_16MHZ;						//run the 16 MHz clock
 	DCOCTL = CALDCO_16MHZ;
 	P1SEL = BIT1 + BIT2 ;                     // P1.1 = RXD, P1.2=TXD
-	P1SEL2 = BIT1 + BIT2 ;                     // P1.1 = RXD, P1.2=TXD
+	P1SEL2 = BIT1 + BIT2 ;                     // P1.1 = RXD, P1.2=TXD, use for serial communication
 	UCA0CTL1 |= UCSSEL_2;                     // SMCLK
 
 	const unsigned long brd = (smclk_freq + (bps >> 1)) / bps; // Bit rate divisor
@@ -68,22 +68,22 @@ void main(void){
 	UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
 	IE2 |= UCA0RXIE;                          // Enable USCI_A0 RX interrupt
 	__bis_SR_register(GIE);
-    P2SEL = 0;
-    P2SEL2 = 0;
+    P2SEL = 0;								// Select all the 2.X pins to general I/O
+    P2SEL2 = 0;								// Select all the 2.X pins to general I/O
 
-    P2DIR = BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
-    P2OUT = BIT0 + BIT2 + BIT4 + BIT6;
+    P2DIR = BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7; 		//Set all the 2.X pins to output
+    P2OUT = BIT0 + BIT2 + BIT4 + BIT6;                                  //Initialize pins 2.0, 2.2, 2.4, and 2.6 to high
 
-    while (1){								//main loop. entered  is sent
+    while (1){								//main loop that will continuously output signal if a state is on
 
-		while (ALL){
+		while (ALL){						// output to all 2.X pins
 				P2OUT ^= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
 				pulse_delay(freq);
 				P2OUT ^= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
 				pulse_delay(freq);
 			}
 
-		while (SYNC1){
+		while (SYNC1){						// output to BNC 1
 				P2OUT ^= BIT4 + BIT5;
 				pulse_delay(freq);
 				P2OUT ^= BIT4 + BIT5;
@@ -91,7 +91,7 @@ void main(void){
 			}
 
 
-		while (SYNC2){
+		while (SYNC2){						// output to BNC 2
 				P2OUT ^= BIT6 + BIT7;
 				pulse_delay(freq);
 				P2OUT ^= BIT6 + BIT7;
@@ -99,42 +99,42 @@ void main(void){
 			}
 
 
-		while (CAM1){
+		while (CAM1){						// output to audio jack 1
 				P2OUT ^= BIT0 + BIT1;
 				pulse_delay(freq);
 				P2OUT ^= BIT0 + BIT1;
 				pulse_delay(freq);
 			}
 
-		while (CAM2){
+		while (CAM2){						// output to audio jack 2
 				P2OUT ^= BIT2 + BIT3;
 				pulse_delay(freq);
 				P2OUT ^= BIT2 + BIT3;
 				pulse_delay(freq);
 			}
 
-		while (SIDE1){
+		while (SIDE1){						// output to 1 audio jack and 2 BNC
 				P2OUT ^= BIT0 + BIT1 + BIT6 + BIT7;
 				pulse_delay(freq);
 				P2OUT ^= BIT0 + BIT1 + BIT6 + BIT7;
 				pulse_delay(freq);
 			}
 
-		while (SIDE2){
+		while (SIDE2){						// output to 1 audio jack and 1 BNC
 				P2OUT ^= BIT2 + BIT3 + BIT4 + BIT5;
 				pulse_delay(freq);
 				P2OUT ^= BIT2 + BIT3 + BIT4 + BIT5;
 				pulse_delay(freq);
 			}
 
-		while (SYNC){
+		while (SYNC){						// output to both BNC
 				P2OUT ^= BIT4 + BIT5 + BIT6 + BIT7;
 				pulse_delay(freq);
 				P2OUT ^= BIT4 + BIT5 + BIT6 + BIT7;
 				pulse_delay(freq);
 			}
 
-		while (CAM){
+		while (CAM){						// output to both audio jacks
 				P2OUT ^= BIT0 + BIT1 + BIT2 + BIT3;
 				pulse_delay(freq);
 				P2OUT ^= BIT0 + BIT1 + BIT2 + BIT3;
@@ -152,9 +152,10 @@ __interrupt void USCI0RX_ISR(void)
 	 	out(UCA0RXBUF);             //return every character received
 
 	    if (UCA0RXBUF == 0x0d){         //if enter is pressed, check to see if a valid command has been input
-	        out('\r');
+	        out('\r');					//return 'Enter'
 	        out('\n');
-
+			
+			//change current frequency, keeps output state the same
 	        if ((test[0] == 'z' || test[0] == 'Z') && (test[1] == 'h' || test[1] == 'H') && (test[2] == '0') && (test[3] == '3')){
 	        	freq = 30;
 	        }
@@ -176,13 +177,14 @@ __interrupt void USCI0RX_ISR(void)
 	        }
 
 	        else{
-	        				ALL = SYNC1 = SYNC2 = CAM1 = CAM2 = SIDE1 = SIDE2 = SYNC = CAM = 0;
+	        				ALL = SYNC1 = SYNC2 = CAM1 = CAM2 = SIDE1 = SIDE2 = SYNC = CAM = 0; // if any command other then changing frequency is pressed, clear all outputs before applying new output state
 
-	        				if ((test[0] == 't'||test[0] == 'T') && (test[1] == 's' || test[1] == 'S') && (test[2] == 'e' || test[2] == 'E') && (test[3] == 't' || test[3] == 'T')){ //outputs '30pulses@"x"hz
+	        				if ((test[0] == 't'||test[0] == 'T') && (test[1] == 's' || test[1] == 'S') && (test[2] == 'e' || test[2] == 'E') && (test[3] == 't' || test[3] == 'T')){ //output test
 	        		        	cameratest();
 	        		        }
-
-	        				else if ((test[0] == 'l'||test[0] == 'L') && (test[1] == 'l' || test[1] == 'L') && (test[2] == 'a' || test[2] == 'A')){		//starts pulses
+							
+							//turn on an output state
+	        				else if ((test[0] == 'l'||test[0] == 'L') && (test[1] == 'l' || test[1] == 'L') && (test[2] == 'a' || test[2] == 'A')){
 	        		        	ALL ^= 1;
 	        		        }
 
